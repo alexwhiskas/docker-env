044dff51046364dd4b2a41adf8e0012a87c9cbc3
Alexander Galaburda <algal@smile.fr>
2022-01-10 16:30:14 +0200

    Dev-Environment, updated mailhog service to store data in volumes dir

--- bin/utility/mysql/dump-import-archived.sh
+++ bin/utility/mysql/dump-import-archived.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 [ -z "$1" ] && echo "Define database name" && exit;
-[ -z "$2" ] && echo "Define dump file path to create" && exit;
+[ -z "$2" ] && echo "Define dump file path to import" && exit;
 
 zcat $2 | ./bin/docker/cli.sh --no-tty mysql mysql -uroot -proot $1;
