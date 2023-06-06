cd4e845e21683d73daab3b85f847de629235f0b6
Alex Halaburda <a.halaburda@youweagency.com>
2023-06-06 12:22:05 +0300

    Docker env: small updates

--- bin/magento/cache-flush.sh
+++ bin/magento/cache-flush.sh
@@ -3,12 +3,12 @@
 source ./.env
 
 if [ "$1" == "--hard" ]; then
-  echo "Flushing magento cache"
-  ./bin/magento/cache-flush.sh
   echo "Removing all generated and cached files"
   (cd $LOCALHOST_PROJECT_PATH && sudo rm -rf generated/* pub/static/* var/cache/* var/view_preprocessed/*);
   echo "Flushing Redis"
   ./bin/docker/redis-flush.sh
+  echo "Flushing magento cache"
+  ./bin/magento.sh cache:flush
 else
 ./bin/magento.sh cache:flush "$@"
 fi
--- bin/utility/ssl-proxy/generate-certificate.sh
+++ bin/utility/ssl-proxy/generate-certificate.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 [ -z "$1" ] && echo "Define domain name" && exit;
+# uncomment line below and move usage of $2 param for multistore purpose
+#[ -z "$2" ] && echo "Define subdomain name" && exit;
 
 openssl req -x509 -newkey rsa:2048 \
     -keyout ./services/ssl-proxy/certificates/self-signed.key \
     -out ./services/ssl-proxy/certificates/self-signed.crt \
     -days 500 \
-    -subj /C=AU/ST=DevEnv/L=Melbourne/O=DevEnv/CN=$1 \
+    -subj /C=AU/ST=DevEnv/L=London/O=DevEnv/CN=$1 \
     -nodes;
+
+# move before "-nodes;" line for multistore purpose
+#    -addext "subjectAltName=DNS:$1, DNS:$2" \
