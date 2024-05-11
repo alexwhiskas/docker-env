1684d20f866d84713a87c00519b07000c67e3577
Alex Halaburda <a.halaburda@youweagency.com>
2024-05-12 00:52:33 +0200

    updates for magento 2.4.6

--- bin/magento/cache-flush.sh
+++ bin/magento/cache-flush.sh
@@ -3,12 +3,14 @@
 source ./.env
 
 if [ "$1" == "--hard" ]; then
-  echo "Removing all generated and cached files"
-  (cd $LOCALHOST_PROJECT_PATH && sudo rm -rf generated/* pub/static/* var/cache/* var/view_preprocessed/*);
   echo "Flushing Redis"
   ./bin/docker/redis-flush.sh
+  echo "Removing all generated and cached files"
+  (cd $LOCALHOST_PROJECT_PATH && sudo rm -rf generated/* pub/static/* var/cache/* var/view_preprocessed/*);
   echo "Flushing magento cache"
   ./bin/magento.sh cache:flush
+  echo "Removing all generated and cached files"
+  (cd $LOCALHOST_PROJECT_PATH && sudo rm -rf generated/* pub/static/* var/cache/* var/view_preprocessed/*);
 else
 ./bin/magento.sh cache:flush "$@"
 fi
