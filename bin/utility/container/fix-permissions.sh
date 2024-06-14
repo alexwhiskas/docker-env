00d5794a7c8a4737cbbf7d46f60cef38458b5deb
Alex Halaburda <a.halaburda@youweagency.com>
2024-06-14 10:59:13 +0200

    Docker env: small updates

--- bin/utility/container/fix-permissions.sh
+++ bin/utility/container/fix-permissions.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+source ./.env
 
-sudo chmod -R 777 ../universal-drugstore-magento
+sudo chmod -R 777 $LOCALHOST_PROJECT_PATH
+
 #(cd ../universal-drugstore-magento && sudo find var vendor pub/static pub/media app/etc \( -type f -or -type d \) -exec chmod u+w {} +);
 #./bin/docker/php.sh chmod u+x bin/magento
